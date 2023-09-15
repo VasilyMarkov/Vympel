dac574fd8a78e78dd7fb38df56296bd35b842c94
V_markov <mvv1993Vasya@gmail.com>
2023-09-15 23:43:03 +0300

    Image processing

---
+++ image_process.py
@@ -0,0 +1,8 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+n = 1000
+t = np.linspace(-3, 3, n)
+x = np.cos(30 * t) + np.sin(10 * t) + 0.3 * (t ** 2)
+
+image = cv2.imread("frame1.jpg", 0)
