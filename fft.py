80352cbbcf98407049333c827d61621e6314a444
Vasily Markov <mvv1993Vasya@gmail.com>
2024-01-15 00:15:32 +0300

    fft

--- fft.py
+++ fft.py
@@ -3,8 +3,22 @@ import numpy as np
 import numpy.fft as fft
 import matplotlib.pyplot as plt
 import time
+import sys
+import os
 
-def proc(cap):
+
+def cutFrame(cap, numFrame):
+    cnt = 0
+    while cap.isOpened(): 
+        ret, frame = cap.read()
+        if cnt == numFrame:            
+            break 
+        cnt += 1
+        if cv.waitKey(1) == ord('q'):
+            break  
+    return frame
+
+def fft_video(cap):
     while cap.isOpened():
         ret, frame = cap.read()
         start = time.time()
@@ -13,10 +27,33 @@ def proc(cap):
         fft_normalized = np.log(1 + abs(fft_centered))
         # cv.imshow('fft', abs(fft_frame))
         end = time.time()
-        print(end-start)
+        print(f'{end-start}, s')
         if cv.waitKey(1) == ord('q'):
             break    
 
-cap = cv.VideoCapture("test2.mp4")
+def fft_img(img):
+    fft_frame = fft.fft2(img)
+    fft_centered = fft.fftshift(fft_frame)
+    fft_normalized = np.log(1 + np.abs(fft_centered)) 
+    return fft_normalized
 
-proc(cap)
+cap = cv.VideoCapture("decan.mp4")
+img = cv.cvtColor(cv.imread('img/decan/decan_150.png'), cv.COLOR_BGR2GRAY)
+
+# total = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+# print(total)
+path = './img/decan/'
+files = [f for f in os.listdir(path)]
+fig, axs = plt.subplots(2, 3)
+files = sorted(files)
+print(files)
+fft_img(cv.cvtColor(cv.imread(f'{path}{files[0]}'), cv.COLOR_BGR2GRAY))
+axs[0, 0].imshow(fft_img(cv.cvtColor(cv.imread(f'{path}{files[0]}'), cv.COLOR_BGR2GRAY)), cmap = 'gray')
+axs[0, 1].imshow(fft_img(cv.cvtColor(cv.imread(f'{path}{files[1]}'), cv.COLOR_BGR2GRAY)), cmap = 'gray')
+axs[0, 2].imshow(fft_img(cv.cvtColor(cv.imread(f'{path}{files[2]}'), cv.COLOR_BGR2GRAY)), cmap = 'gray')
+axs[1, 0].imshow(fft_img(cv.cvtColor(cv.imread(f'{path}{files[3]}'), cv.COLOR_BGR2GRAY)), cmap = 'gray')
+axs[1, 1].imshow(fft_img(cv.cvtColor(cv.imread(f'{path}{files[4]}'), cv.COLOR_BGR2GRAY)), cmap = 'gray')
+# axs[0, 0].imshow(fft_img(img), cmap = 'gray')
+# plt.imshow(fft_img(img), cmap='gray')
+plt.show()
+cv.waitKey(0) 
