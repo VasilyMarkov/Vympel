2b1c3d8bd5c39fa1983ff072a17b0b4d71f3e1ed
VasilyMarkov <mvv1993vasya@gmail.com>
2025-02-27 12:33:07 +0000

    fix optimization script

--- prod/optimization.py
+++ prod/optimization.py
@@ -4,27 +4,6 @@ import sys
 import struct
 from scipy.signal import butter, filtfilt
 
-def butter_lowpass(cutoff, fs, order=5):
-    nyquist = 0.5 * fs
-    normal_cutoff = cutoff / nyquist
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
-    return b, a
-
-
-fs = 500.0  # Sampling frequency
-cutoff = 20.0  # Desired cutoff frequency of the filter, Hz
-order = 5  # Order of the filter
-
-def lowpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_lowpass(cutoff, fs, order=order)
-    y = filtfilt(b, a, data)
-    return y
-
-def genGaussianData(coeffs, sigma, x):
-    func = coeffs[0]*np.exp(-0.5 * ((x - coeffs[1]) / coeffs[2])**2)
-    random_data = np.random.normal(0, sigma/5, x.shape[0])
-    return func + random_data
-
 def polynomial(x, coeffs):
     return np.polyval(coeffs, x)
 
@@ -34,14 +13,6 @@ def gaussian(x, a, x0, sigma, poly_coeffs):
 
     return a * np.exp((-1/2) * (polynomial_value / sigma) ** 2)
 
-def normalize(data):
-    data -= np.min(data)
-    data /= np.max(data)
-    return data
-
-
-x_data = []
-y_data = []
 
 def objective_function(params):
     a = params[0]
@@ -50,6 +21,11 @@ def objective_function(params):
     polyCoeffs = params[3:]
     y_pred = gaussian(x_data, a, x0, sigma, polyCoeffs)
     return np.sum((y_data - y_pred)**2)
+
+def normalize(data):
+    data -= np.min(data)
+    data /= np.max(data)
+    return data
 
 def read_exact(n):
     data = b''
@@ -64,29 +40,47 @@ def readData():
     size_data = read_exact(4)
     size = struct.unpack('!I', size_data)[0]
     double_data = read_exact(size * 8)
-    return struct.unpack(f'!{size}d', double_data)
+    y_data = struct.unpack(f'!{size}d', double_data)
+    y_data = normalize(y_data)
+    x_data = np.linspace(0, len(y_data)-1, len(y_data))
+    return x_data, y_data
 
 def writeData(data):
-    output_data = struct.pack('!I', len(data))  # Size
-    output_data += struct.pack(f'!{len(data)}d', *data)  # Data
+    output_data = struct.pack('!I', len(data))  
+    output_data += struct.pack(f'!{len(data)}d', *data)  
     sys.stdout.buffer.write(output_data)
     sys.stdout.buffer.flush()
 
-def main():
+
+def findOptimum(x_data, y_data):
+    scale = 1
+    guessShift = np.argmax(y_data)
+    variance = 1
+    a = 1
+    b = 1
+    c = 1
+    initCoefficients = [scale, guessShift, variance, a, b, c] #ax^2+bx+c
+    resultsCoefficients = []
+    stdDevList = []
+    for _ in range(8):
+        result = minimize(objective_function, initCoefficients, method='Powell').x
+        resultsCoefficients.append(result)
+        a = result[0]
+        x0 = result[1]
+        sigma = result[2]
+        polyCoeffs = result[3:]       
+        res_gaussian = gaussian(x_data, a, x0, sigma, polyCoeffs)
+        stdDev = np.sqrt(np.mean((np.array(y_data) - np.array(res_gaussian))**2))
+        stdDevList.append(stdDev)
+        initCoefficients.append(1)
+
+    minStdDevIndex = np.argmin(stdDevList)
+    return resultsCoefficients[minStdDevIndex]
+
+if __name__ == "__main__":
     try:
-        y_data = lowpass_filter(readData(), cutoff, fs, order)
-        y_data = normalize(y_data)
-        x_data = np.linspace(0, len(y_data)-1, len(y_data))
-        guess_shift = np.argmax(y_data)
-        initial_guess = [1, guess_shift, 1, 1, 1, 1] #x^3+x^2+x+a
-        result = minimize(objective_function, initial_guess, method='Powell').x
-
-        writeData(result)
-
+        x_data, y_data = readData()
+        writeData(findOptimum(x_data, y_data))
     except Exception as e:
         sys.stderr.write(f"Error: {str(e)}")
         sys.exit(1)
-
-if __name__ == "__main__":
-    main()
-# func = gaussian(result, x_data)
